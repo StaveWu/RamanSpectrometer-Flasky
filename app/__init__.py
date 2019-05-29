3de2bca759d3b7ccc9c59095eb6ad7f83fd26204
腾达 吴 <wtengda0816@outlook.com>
2019-05-29 20:03:44 +0800

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from .io import SpectrumIO, ComponentIO, ModelIO
@@ -11,6 +12,7 @@ model_io = ModelIO()
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
