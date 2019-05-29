a2b2838af27a2c80620d2d009b8b0adf46176d8d
腾达 吴 <wtengda0816@outlook.com>
2019-05-29 19:00:30 +0800

    fill dae function

--- app/algorithms/denoise.py
+++ app/algorithms/denoise.py
@@ -1,8 +1,22 @@
 from scipy.signal import savgol_filter
+from tensorflow import keras
+import numpy as np
 
 
 def dae(x):
-    pass
+    x = np.array(x)
+    dims = x.ndim
+    if dims == 1:
+        x = x.reshape(-1, 1)
+    if dims == 2:
+        x = np.expand_dims(x, axis=2)
+    model = keras.models.load_model('./dae.h5')
+    res = model.predict(x)
+    if dims == 2:
+        res = np.squeeze(res, axis=2)
+    if dims == 1:
+        res = res.flatten()
+    return res
 
 
 def wavelet(x):
