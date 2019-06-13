df61dd590ca74409075f13336f46a0020cfdc81c
腾达 吴 <wtengda0816@outlook.com>
2019-06-13 19:02:33 +0800

    remove venv for simply use. add run.bat

--- boot.bat
+++ boot.bat
@@ -2,7 +2,5 @@ echo FLASK_APP=flasky>.env
 echo FLASK_ENV=production>>.env
 echo FLASK_CONFIG=production>>.env
 
-python -m venv venv
-%CD%/venv/Scripts/activate.bat
 pip install -r requirements/common.txt
 python reset.py
