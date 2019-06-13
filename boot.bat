a2fbf5e84b8a2ee23e5d876c2c6dcc5bb41eaded
腾达 吴 <wtengda0816@outlook.com>
2019-06-13 16:29:59 +0800

    fix boot bug

--- boot.bat
+++ boot.bat
@@ -3,6 +3,6 @@ echo FLASK_ENV=production>>.env
 echo FLASK_CONFIG=production>>.env
 
 python -m venv venv
-venv/Scripts/activate.bat
+%CD%/venv/Scripts/activate.bat
 pip install -r requirements/common.txt
 python reset.py
